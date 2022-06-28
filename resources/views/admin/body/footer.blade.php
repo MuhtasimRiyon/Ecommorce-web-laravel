0e997ff801c438346de8dc06012e10834f6a7f3f
Muhtasim <57425467+MuhtasimRiyon@users.noreply.github.com>
2022-06-28 17:50:22 +0600

    Admin dashboard and admin profile load

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,14 @@
+
+<footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+		  <li class="nav-item">
+			<a class="nav-link" href="javascript:void(0)">FAQ</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="#">MR13 dev.com</a>
+		  </li>
+		</ul>
+    </div>
+	  &copy; 2022 <a href="#">MR13 dev</a> All Rights Reserved.
+</footer>
